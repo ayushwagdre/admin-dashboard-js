ac924a9f05a710bcfb9000197fee35e2fe48673c
ayushwagdre <ayushwagdre05@gmail.com>
2025-11-26 15:54:04 +0530

    fix

--- src/api/client.js
+++ src/api/client.js
@@ -33,102 +33,102 @@ class ApiClient {
 
   // User endpoints
   async getUsers() {
-    const response = await this.client.get('/users/');
+    const response = await this.client.get('/users');
     return response.data;
   }
 
   async getUserById(id) {
-    const response = await this.client.get(`/users/${id}/`);
+    const response = await this.client.get(`/users/${id}`);
     return response.data;
   }
 
   async createUser(user) {
-    const response = await this.client.post('/users/', user);
+    const response = await this.client.post('/users', user);
     return response.data;
   }
 
   async updateUser(id, user) {
-    const response = await this.client.put(`/users/${id}/`, user);
+    const response = await this.client.put(`/users/${id}`, user);
     return response.data;
   }
 
   async deleteUser(id) {
-    await this.client.delete(`/users/${id}/`);
+    await this.client.delete(`/users/${id}`);
   }
 
   // Blog endpoints
   async getBlogs() {
-    const response = await this.client.get('/blogs/');
+    const response = await this.client.get('/blogs');
     return response.data;
   }
 
   async getBlogById(id) {
-    const response = await this.client.get(`/blogs/${id}/`);
+    const response = await this.client.get(`/blogs/${id}`);
     return response.data;
   }
 
   async createBlog(blog) {
-    const response = await this.client.post('/blogs/', blog);
+    const response = await this.client.post('/blogs', blog);
     return response.data;
   }
 
   async updateBlog(id, blog) {
-    const response = await this.client.put(`/blogs/${id}/`, blog);
+    const response = await this.client.put(`/blogs/${id}`, blog);
     return response.data;
   }
 
   async deleteBlog(id) {
-    await this.client.delete(`/blogs/${id}/`);
+    await this.client.delete(`/blogs/${id}`);
   }
 
   // Portfolio endpoints
   async getPortfolios() {
-    const response = await this.client.get('/portfolios/');
+    const response = await this.client.get('/portfolios');
     return response.data;
   }
 
   async getPortfolioById(id) {
-    const response = await this.client.get(`/portfolios/${id}/`);
+    const response = await this.client.get(`/portfolios/${id}`);
     return response.data;
   }
 
   async createPortfolio(portfolio) {
-    const response = await this.client.post('/portfolios/', portfolio);
+    const response = await this.client.post('/portfolios', portfolio);
     return response.data;
   }
 
   async updatePortfolio(id, portfolio) {
-    const response = await this.client.put(`/portfolios/${id}/`, portfolio);
+    const response = await this.client.put(`/portfolios/${id}`, portfolio);
     return response.data;
   }
 
   async deletePortfolio(id) {
-    await this.client.delete(`/portfolios/${id}/`);
+    await this.client.delete(`/portfolios/${id}`);
   }
 
   // Testimonial endpoints
   async getTestimonials() {
-    const response = await this.client.get('/testimonials/');
+    const response = await this.client.get('/testimonials');
     return response.data;
   }
 
   async getTestimonialById(id) {
-    const response = await this.client.get(`/testimonials/${id}/`);
+    const response = await this.client.get(`/testimonials/${id}`);
     return response.data;
   }
 
   async createTestimonial(testimonial) {
-    const response = await this.client.post('/testimonials/', testimonial);
+    const response = await this.client.post('/testimonials', testimonial);
     return response.data;
   }
 
   async updateTestimonial(id, testimonial) {
-    const response = await this.client.put(`/testimonials/${id}/`, testimonial);
+    const response = await this.client.put(`/testimonials/${id}`, testimonial);
     return response.data;
   }
 
   async deleteTestimonial(id) {
-    await this.client.delete(`/testimonials/${id}/`);
+    await this.client.delete(`/testimonials/${id}`);
   }
 }
 
